d1f91e2d563010a4d1e8176852ca8050cb5ae638
tommy <tommy@Thomass-MacBook-Pro-2.local>
2024-02-12 21:27:37 -0800

    updating more files

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
-mport {AddConsumer, CreateSubscription, FundSubscription} from "./Interactions.s.sol";
+import {AddConsumer, CreateSubscription, FundSubscription} from "./Interactions.s.sol";
 
 contract DeployRaffle is Script {
     function run() external returns (Raffle, HelperConfig){
